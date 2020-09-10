f27841fa4c6e90b3f49414265ddb796902cee769
michael-work <michaelkushnir123233@gmail.com>
2020-09-10 15:44:13 +0300

    test successful

--- app.py
+++ app.py
@@ -232,7 +232,6 @@ def score():
             return render_template('index.html', model_names=model_names, labels=model_labels)
         except Exception as e:
             flash('Something went wrong')
-            print('hgelo')
             app.logger.error(e)
             return render_template('index.html', model_names=model_names, labels=model_labels)
     return render_template("index.html", model_names=model_names, labels=model_labels)
